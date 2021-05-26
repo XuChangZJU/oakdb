61879a2a020ce2c86ed7ced84fe21e6ce84f146b
Xc@centOs <chinawraith@163.com>
2021-05-26 13:28:29 +0800

    增加了judgeRelation函数

--- lib/oakDb.d.ts
+++ lib/oakDb.d.ts
@@ -126,4 +126,18 @@ export declare class OakDb extends Warden {
         query?: Query;
         txn?: Txn;
     }): Promise<void>;
+    /**
+     * @description judge relation of attr to entity
+     * @param entity
+     * @param attr
+     * @returns {
+     *      1: many-to-one,
+     *      2: many-to-one(using entity/entityId pointer)
+     *      []: one-to-many(using entity as attribute name)
+     *      {}: one-to-many(using entity/entityId)
+     * }
+     */
+    judgeRelation(entity: string, attr: string): string | string[] | 1 | 2 | {
+        $$entity: string;
+    };
 }
