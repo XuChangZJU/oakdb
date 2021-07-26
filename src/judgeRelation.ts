a7bcf47a6847af7cf540c27bc45fcbb31a98f7ca
Xc@centOs <chinawraith@163.com>
2021-07-26 14:01:57 +0800

    允许对同一个对象产生多个一对多的级联映射

--- src/judgeRelation.ts
+++ src/judgeRelation.ts
@@ -34,8 +34,17 @@ export function judgeRelation(entity: string, attr: string, schema: Schema): any
         return 2;       // entity指针的多对一
     }
     else {
-        assert(attr.endsWith('s'));
-        const attr2 = attr.slice(0, attr.length - 1);
+        /**
+         * 传入的格式可能是：{
+         *      qiniuFiles$1:{
+         *      },
+         *      qiniuFiles$2: {
+         *      },
+         * }
+         */
+        const attr3 = attr.split('$')[0];
+        assert(attr3.endsWith('s'));
+        const attr2 = attr3.slice(0, attr3.length - 1);
         const { attributes: attributes2 } = schema[attr2];
         if (attributes2.hasOwnProperty(entity)) {
             // 此时要求定义的时候一定要按entity名称来定义属性，如果有多个属性映射成外键是不能支持的  by Xc
